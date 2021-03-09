e62c6ea1133f4d09e171d648e887a29db330a0df
José Daniel Silva do Carmo <dan321daniel@gmail.com>
2021-03-09 19:49:32 -0300

    Servidor, uso de node.js, npm e nunjucks.

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const server = express()
+//.use faz a pasta public ficar como se estivesse junto do views
+server.use(express.static("public"))
+//Template engine para deixar o html dinâmico
+const nunjucks = require ("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+//req: requisição
+//res: resposta
+//__dirname é ir no directório
+server.get("/",(rec, res) => {
+    return res.render("index.html")
+})
+//render funciona quando se põe template, caso contrário seria res.sendFile(__dirname+)
+server.get("/create-point",(rec, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search-results",(rec, res) => {
+    return res.render("search-results.html")
+})
+server.listen(3000)
